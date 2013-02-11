b171691a2ffbcd03457e3f80af15859c4a31f9e8
Joel B. Greenberg <joelgreenberg26@gmail.com>
2013-02-10 21:55:51 -0500

    Changes Scala (concrete) type to interface type.

--- src/com/jbhg/java/Main.java
+++ src/com/jbhg/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 		IWriter w1 = new WriterImpl();
 		System.out.println(w1.write());
-		WriterImplScala w2 = new WriterImplScala();
+		IWriter w2 = new WriterImplScala();
 		System.out.println(w2.write());
 
 		System.out.println("Positive: " + w2.getIsPositive(5));
